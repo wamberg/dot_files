e8ae5425be6f5893a9c1205da0a4318de43ef750
Bill Amberg <wamberg@hearth.works>
2022-06-28 14:31:05 -0400

    Manage lua LSP with nix

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Tend to my synapses with non-linear notes";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable }:
+    let
+
+      # to work with older version of flakes
+      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+
+      # Generate a user-friendly version number.
+      version = builtins.substring 0 8 lastModifiedDate;
+
+      # System types to support.
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Overlays
+      overlay-unstable = final: prev: {
+        unstable = inputs.nixpkgs-unstable.legacyPackages.x86_64-linux;
+      };
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs {
+        inherit system;
+        config = { allowUnfree = true; };
+        overlays =
+          [ overlay-unstable ];
+      });
+
+    in {
+      devShell = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in pkgs.mkShell {
+          buildInputs = with pkgs; [
+            sumneko-lua-language-server
+          ];
+        });
+    };
+}
