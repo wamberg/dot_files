ef584c2e9cdf6da3b59761ca65250bdc72b527ad
Bill Amberg <wamberg@hearth.works>
2022-07-18 10:18:51 -0400

    nvim - scrolloff = 2

--- nvim/.config/lvim/config.lua
+++ nvim/.config/lvim/config.lua
@@ -12,7 +12,7 @@ vim.opt.foldlevel = 99
 vim.opt.foldmethod = "manual"
 vim.opt.mouse = "" -- disable mouse
 vim.opt.relativenumber = true
-vim.opt.scrolloff = 4
+vim.opt.scrolloff = 2
 vim.opt.wrap = true
 
 ----------------------
