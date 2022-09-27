b02cee7c669484dacf3e4f7af52465532e5fb255
Bill Amberg <wamberg@hearth.works>
2022-09-27 13:17:18 -0400

    Create gnome42Extensions.paperwm for latest paperwm

--- nix/overlays/paperwm.nix
+++ nix/overlays/paperwm.nix
@@ -1,13 +1,20 @@
-(self: super: {
-  gnomeExtensions = super.gnomeExtensions // {
-    paperwm = super.gnomeExtensions.paperwm.overrideDerivation (old: {
-      version = "42.0";
-      src = super.fetchFromGitHub {
-        owner = "PaperWM-community";
-        repo = "PaperWM";
-        rev = "gnome-42";
-        sha256 = "sha256-dBN5CdxUzF7UqIRYjfT5EUCetHs1ZD+1KCNv3oSMRuE=";
-      };
+# Thank you, terlar
+# https://github.com/terlar/nix-config/blob/9c4f5cc59dabb83249dfd0e984a889bc4287ef1f/overlays/gnome-extensions/default.nix
+final: prev: {
+  gnome42Extensions =
+    prev.gnome42Extensions
+    // (with final.gnome; {
+      paperwm = prev.gnomeExtensions.paperwm.overrideDerivation (old: let
+        version = "42.0";
+      in {
+        inherit version;
+        name = "${old.pname}-${version}";
+        src = prev.fetchFromGitHub {
+          owner = "paperwm";
+          repo = "paperwm";
+          rev = "3b7a4b6c07512d3ba5e1967cda0fbe63c6bb0ae1";
+          hash = "sha256-HuqSp6NM9Ye9SyQT+il5Cn4FsSxnT6CAlA/NjwBkajo=";
+        };
+      });
     });
-  };
-})
+}
