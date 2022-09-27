02f6d23d05ea802d904df2dcab85980b0a849148
Bill Amberg <wamberg@hearth.works>
2022-09-27 13:02:19 -0400

    Non-working, but progress, paperwm overlay

--- nix/overlays/paperwm.nix
+++ nix/overlays/paperwm.nix
@@ -1,14 +1,13 @@
 (self: super: {
   gnomeExtensions = super.gnomeExtensions // {
     paperwm = super.gnomeExtensions.paperwm.overrideDerivation (old: {
-      version = "pre-41.0";
+      version = "42.0";
       src = super.fetchFromGitHub {
         owner = "PaperWM-community";
         repo = "PaperWM";
-        rev = "next-release";
-        sha256 = "6AUUu63oWxRw9Wpxe0f7xvt7iilvQfhpAB8SYG4yP8Q=";
+        rev = "gnome-42";
+        sha256 = "sha256-dBN5CdxUzF7UqIRYjfT5EUCetHs1ZD+1KCNv3oSMRuE=";
       };
-      patches = old.patches ++ [ ../patches/paperwm.patch ];
     });
   };
 })
