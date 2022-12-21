2712853d25836bdae259ed961e4d2b75f3d3712d
Bill Amberg <wamberg@hearth.works>
2022-12-21 18:36:39 -0500

    Add zen mode and vimwiki header highlighting

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,10 +1,31 @@
 require("kickstart.init")
 
+-- line numbers
 vim.opt.cursorline = true
 vim.opt.cursorlineopt = "number"
+vim.opt.relativenumber = true
+
+-- folds
 vim.opt.foldlevel = 99
 vim.opt.foldmethod = "manual"
-vim.opt.mouse = "" -- disable mouse
-vim.opt.relativenumber = true
+
+-- display
 vim.opt.scrolloff = 2
+vim.opt.splitbelow = true
+vim.opt.splitright = true
 vim.opt.wrap = true
+
+-- Toggle Zen mode
+vim.keymap.set('n', '<leader>z', require('zen-mode').toggle, { desc = '[Z]en Mode' })
+
+-- vimwiki specific highlighting
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "vimwiki",
+  callback = function()
+    vim.cmd [[highlight VimwikiHeader1 guifg=blue gui=bold]]
+    vim.cmd [[highlight VimwikiHeader2 guifg=darkgreen gui=bold]]
+    vim.cmd [[highlight VimwikiHeader3 guifg=darkorange3 gui=bold]]
+    vim.cmd [[highlight VimwikiHeader4 guifg=magenta3 gui=bold]]
+    vim.cmd [[highlight VimwikiHeader5 guifg=magenta gui=bold]]
+  end,
+})
