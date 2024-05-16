64918bbdb3a8f284255d8b63d91267303a883b00
Bill Amberg <wamberg@hearth.works>
2024-05-16 09:12:21 -0400

    nvim - show extendend autocomplete text

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -399,9 +399,9 @@ require("lazy").setup({
           --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
         }),
         sources = {
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-          { name = "path" },
+          { name = "nvim_lsp", group_index = 2 },
+          { name = "luasnip", group_index = 2 },
+          { name = "path", group_index = 2 },
         },
       })
     end,
