853ed3a5e7e7ce726e9128bc7936928ca0c95ffe
Bill Amberg <wamberg@hearth.works>
2024-06-26 11:31:43 -0400

    nvim - change Code Action keyboard shortcut

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -169,7 +169,7 @@ require("lazy").setup({
 
           -- Execute a code action, usually your cursor needs to be on top of an error
           -- or a suggestion from your LSP for this to activate.
-          map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+          map("<leader>oa", vim.lsp.buf.code_action, "C[o]de [A]ction")
 
           -- Opens a popup that displays documentation about the word under your cursor
           --  See `:help K` for why this keymap.
