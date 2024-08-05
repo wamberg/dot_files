2c39ad32cfdaea17e1739709279cafeaa4289c70
Bill Amberg <wamberg@hearth.works>
2024-08-05 08:06:14 -0400

    nvim - configure htmx lsp for htmldjango

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -215,19 +215,9 @@ require("lazy").setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
+        htmx = { filetypes = { "html", "htmldjango" } },
         pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
         tsserver = {},
-        --
-
         lua_ls = {
           -- cmd = {...},
           -- filetypes = { ...},
