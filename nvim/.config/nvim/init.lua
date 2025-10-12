9992d37e094c9815234140b0bc3b6ce18def1718
Bill Amberg <wamberg@hearth.works>
2025-10-12 11:49:51 -0400

    nvim - update deps

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -279,7 +279,7 @@ require("lazy").setup({
       formatters_by_ft = {
         lua = { "stylua" },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        -- python = { "isort", "ruff_format" },
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
