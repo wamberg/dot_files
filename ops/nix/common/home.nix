214541b248b7bfb94f09f910bed6c1fa26913786
Bill Amberg <wamberg@hearth.works>
2025-12-22 11:54:22 -0500

    nix - embrace default starship layout

--- ops/nix/common/home.nix
+++ ops/nix/common/home.nix
@@ -53,54 +53,15 @@
     enable = true;
     enableZshIntegration = true;
     settings = {
-      "$schema" = "https://starship.rs/config-schema.json";
-
-      format = "$directory$git_branch$git_state$git_status$cmd_duration$line_break$python$character";
-
-      directory = {
-        style = "bold #bd93f9";
-        truncation_length = 0;
-        truncate_to_repo = false;
-      };
-
-      character = {
-        success_symbol = "[❯](#ff79c6)";
-        error_symbol = "[❯](#ff5555)";
-        vimcmd_symbol = "[❮](#50fa7b)";
-      };
-
-      git_branch = {
-        format = "[$branch]($style)";
-        style = "#6272a4";
-      };
-
-      git_status = {
-        format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)]($style) ($ahead_behind$stashed)]($style)";
-        style = "#ff79c6";
-        conflicted = "​";
-        untracked = "​";
-        modified = "​";
-        staged = "​";
-        renamed = "​";
-        deleted = "​";
-        stashed = "≡";
-      };
-
-      git_state = {
-        format = ''\([$state( $progress_current/$progress_total)]($style)\) '';
-        style = "#6272a4";
-      };
-
-      cmd_duration = {
-        format = "[$duration]($style) ";
-        style = "#f1fa8c";
-      };
-
-      python = {
-        format = "[$virtualenv]($style) ";
-        style = "#6272a4";
-        detect_extensions = [];
-        detect_files = [];
+      aws.style = "bold #ffb86c";
+      cmd_duration.style = "bold #f1fa8c";
+      directory.style = "bold #50fa7b";
+      hostname.style = "bold #ff5555";
+      git_branch.style = "bold #ff79c6";
+      git_status.style = "bold #ff5555";
+      username = {
+        format = "[$user]($style) on ";
+        style_user = "bold #bd93f9";
       };
     };
   };
